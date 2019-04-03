104a3d9fc2976c3b2b4807ace913d36ed05cbd22
Lepris <karolina.jankowska@gmail.com>
2019-04-04 00:19:32 +0100

    corrected automatic comment author handling

--- src/components/ArticlePostComment.jsx
+++ src/components/ArticlePostComment.jsx
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 
 class ArticlePostComment extends Component {
     state = {
-        input: {
-            author: 'lepris',
-            body: 'this very test comment',
-        }
+
+    }
+
+    componentDidMount() {
+        const { username } = this.props
+        this.setState({ input: { author: username } })
     }
 
     handleBodyChange = (e) => {
         e.preventDefault()
-        this.setState({ input: { body: e.target.value } })
-    }
-
-    handleAuthorChange = (e) => {
-        e.preventDefault()
-        this.setState({ input: { author: e.target.value } })
+        const commentBody = e.target.value
+        this.setState((prevState, value) => ({
+            input: {
+                author: prevState.input.author,
+                body: commentBody
+            }
+        }));
     }
 
     handleSubmit = (e) => {
@@ -24,15 +27,14 @@ class ArticlePostComment extends Component {
         handlePost(this.state.input)
         this.setState({
             input: {
-                author: 'lepris',
-                body: 'this very test comment',
+                author: '',
+                body: '',
             }
         })
     }
 
     render() {
         return (<form>
-            <input type='text' defaultValue='Username' onChange={this.handleAuthorChange}></input>
             <input type='text' defaultValue='Comment Body' onChange={this.handleBodyChange}></input>
             <button type='submit' onClick={this.handleSubmit}>Add</button>
         </form>)
