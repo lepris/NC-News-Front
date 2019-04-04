f6d91aa49445f0b0d504b365a538eb30605b1f4c
Lepris <karolina.jankowska@gmail.com>
2019-04-04 15:14:51 +0100

    error handling in POST Comment

--- src/components/ArticlePostComment.jsx
+++ src/components/ArticlePostComment.jsx
@@ -1,36 +1,44 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import './ArticlePostComment.css'
 class ArticlePostComment extends Component {
     state = {
         author: '',
-        tempBody: ''
-    }
-
-    componentDidMount() {
-        const { username } = this.props
-        this.setState({ author: username })
+        tempBody: '',
+        typingErr: true,
     }
 
     handleBodyChange = (e) => {
         e.preventDefault()
+        if (this.state.tempBody.length > 10) this.setState({ typingErr: false })
+        if (this.state.tempBody.length < 10) this.setState({ typingErr: true })
+
         this.setState({ tempBody: e.target.value });
     }
 
     handleSubmit = (e) => {
+
         e.preventDefault()
-        const { handlePost } = this.props
+        const { handlePost, username } = this.props
         const input = {
             body: this.state.tempBody,
-            author: this.state.author
+            author: username
         }
+
         handlePost(input)
-        this.setState({ author: '', tempBody: '' })
+        this.setState({ author: '', tempBody: '', typingErr: true })
     }
 
     render() {
         return (<form>
-            <input type='text' placeholder='Comment Body' value={this.state.tempBody} onChange={this.handleBodyChange}></input>
-            <button type='submit' onClick={this.handleSubmit}>Add</button>
+            <h3>NEW COMMENT:</h3>
+
+            <textarea type='text' placeholder='Comment Body' value={this.state.tempBody} onChange={this.handleBodyChange}></textarea>
+            {!this.state.typingErr ?
+                <span className='error_message success'> Valid comment</span>
+                :
+                <span className='error_message fail'> comment is too short :(</span>
+            }
+            <button type='submit' disabled={this.state.typingErr} onClick={this.handleSubmit}>Add</button>
         </form>)
     }
 }
