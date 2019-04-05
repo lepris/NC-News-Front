3c4e7060a05a0aece2a8536a6951fa2062354371
Lepris <karolina.jankowska@gmail.com>
2019-04-05 08:47:53 +0100

    error page improvements

--- src/components/Erroneous.jsx
+++ src/components/Erroneous.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import errorIllustration from '../db/img/error.jpg';
+import './errors.css';
 
 export const Erroneous = ({ message }) => {
     return (
-        <div>
-            <h2>Big Fat Error</h2>
+        <div className='container'>
+            <div className='image'>
+                <img alt='Life of Brian' src={errorIllustration} />
+            </div>
+            <div className='text_part'>
+                <h2>Big Fat Error</h2>
 
-            <h4>{message}</h4>
-            <p>Some things in life are bad,</p>
-            <p>They can really make you mad,</p>
-            <p>Other things just make you swear and curse,</p>
-            <p>When you're chewing life's gristle,</p>
-            <p>Don't grumble,</p>
-            <p>Give a whistle</p>
-            <p><small>-Monthy Python</small></p>
+                <h4>{message}</h4>
+                <p>Some things in life are bad,</p>
+                <p>They can really make you mad,</p>
+                <p>Other things just make you swear and curse,</p>
+                <p>When you're chewing life's gristle,</p>
+                <p>Don't grumble,</p>
+                <p>Give a whistle</p>
+                <p><small>-Monthy Python</small></p>
+            </div>
         </div >
     )
 }
