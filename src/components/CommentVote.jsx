21c0d64af7845979f985e2f45335ef7b73fde5ea
Lepris <karolina.jankowska@gmail.com>
2019-04-10 22:13:38 +0100

    fixing Voting buttons styling

--- src/components/CommentVote.jsx
+++ src/components/CommentVote.jsx
@@ -1,39 +1,62 @@
-import React, { Component } from 'react';
-import { commentVoteUp, commentVoteDown } from '../db/api';
-import './CommentVote.css';
-class CommentVote extends Component {
-    state = {
-        comment: {},
-        votesInc: 0,
-        votedUp: false,
-        votedDown: false,
-    }
+import React, { Component } from "react";
+import { voteUp, voteDown } from "../db/api";
 
-    componentDidMount() {
-        const { comment } = this.props
-        this.setState({ comment })
-    }
+import "./CommentVote.css";
+class Voter extends Component {
+  state = {
+    comment: {},
+    votesInc: 0,
+    votedUp: false,
+    votedDown: false
+  };
 
-    handleVoteUp = () => {
-        commentVoteUp(this.state.comment.comment_id)
-            .then((votes) => this.setState({ votesInc: 1, votedUp: true, votedDown: false }))
-    }
-    handleVoteDown = () => {
-        commentVoteDown(this.state.comment.comment_id)
-            .then((votes) => this.setState({ votesInc: -1, votedDown: true, votedUp: false }))
-    }
+  componentDidMount() {
+    const { comment } = this.props;
+    this.setState({ comment });
+  }
 
-    render() {
+  handleVoteUp = () => {
+    voteUp("comments", this.state.comment.comment_id).then(votes =>
+      this.setState({ votesInc: 1, votedUp: true, votedDown: false })
+    );
+  };
+  handleVoteDown = () => {
+    voteDown("comments", this.state.comment.comment_id).then(votes => {
+      if (votes <= 0) {
+        voteUp("comments", this.state.comment.comment_id);
+        this.setState({ votesInc: 0, votedDown: true, votedUp: false });
+      } else {
+        this.setState({ votesInc: -1, votedDown: true, votedUp: false });
+      }
+    });
+  };
 
-        return (<>
-            <p>
-                <button className='vote' hidden={this.state.votedUp === true} onClick={this.handleVoteUp}>vote<i class="fas fa-thumbs-up"></i></button>
-                <big className='votes_number'>{this.state.comment.votes + this.state.votesInc}</big>
-                <button className='vote' hidden={this.state.votedDown === true} onClick={this.handleVoteDown}>Vote <i class="fas fa-thumbs-down"></i></button>
-            </p>
-
-        </>)
-    }
+  render() {
+    return (
+      <>
+        <p>
+          <button
+            className="vote"
+            disabled={this.state.votedUp === true}
+            onClick={this.handleVoteUp}
+          >
+            vote
+            <i className="fas fa-thumbs-up" />
+          </button>
+          <big className="votes_number">
+            {this.state.comment.votes + this.state.votesInc}
+          </big>
+          <button
+            className="vote"
+            disabled={this.state.votedDown === true}
+            onClick={this.handleVoteDown}
+          >
+            Vote <i className="fas fa-thumbs-down" />
+          </button>
+        </p>
+      </>
+    );
+  }
 }
 
-export default CommentVote
+export default Voter;
