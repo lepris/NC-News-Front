a3a86e1ed3665216847a3f09f01a7a8a1866ddce
Lepris <karolina.jankowska@gmail.com>
2019-04-11 10:30:24 +0100

    article author image error for empty links removed

--- src/components/ArticleAuthor.jsx
+++ src/components/ArticleAuthor.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { fetchUserByUsername } from '../db/api'
 import './ArticleAuthor.css'
+
 class ArticleAuthor extends Component {
     state = {
         authorData: {}
@@ -18,7 +19,7 @@ class ArticleAuthor extends Component {
         console.log(this.state.authorData)
         return (
             <div className='article_author_container'>
-                <div className='avatar_big'><img alt='Article authors avatar' src={this.state.authorData.avatar_url} /></div>
+                <div className='avatar_big'><img alt='Article authors avatar' src={this.state.authorData.avatar_url || null} /></div>
                 <div className='article_author_name'>{this.state.authorData.name}</div>
             </div>)
     }
