78e132343b7ba3a7fd6de3575cf1da355ddc8edb
Lepris <karolina.jankowska@gmail.com>
2019-05-08 00:00:22 +0100

    got rid of description

--- src/components/layout/LeftTopicsPanel.jsx
+++ src/components/layout/LeftTopicsPanel.jsx
@@ -4,7 +4,7 @@ import { Router, Link } from '@reach/router';
 
 export const LeftTopicsPanel = ({ topic }) => (
     <>
-        <span>this new code</span>
+
         <Router className='App-side-route'  >
             <TopicsPanel path='/' />
             <TopicsPanel path='/articles/:articleId' topic={topic} />
