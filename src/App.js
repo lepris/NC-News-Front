1497037314f1d9be9271d6cd6ab6439e5f8cbd6e
Lepris <karolina.jankowska@gmail.com>
2019-05-07 21:19:20 +0100

    experiments with Routers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SingleArticleView from './components/SingleArticleView';
 import LoginBox from './components/LoginBox';
 import AddArticle from './components/AddArticle';
 import { Erroneous } from './components/Erroneous';
+import TopicsPanel from './components/TopicsPanel';
 
 class App extends Component {
   state = {
@@ -27,6 +28,11 @@ class App extends Component {
           {this.state.username && <h5>Logged in as {this.state.username}
             <Link to='/articles/add'> <i className="fas fa-pencil-alt"></i>Add Article</Link></h5>}
         </nav>
+        <span>this new code</span>
+        <Router className='App-side-route'  >
+          <TopicsPanel path='/' />
+          <TopicsPanel path='/topics/:topic' />
+        </Router>
 
         <Router className='App-main-route'  >
           <AddArticle username={this.state.username} path='/articles/add' />
