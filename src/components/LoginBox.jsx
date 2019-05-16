d9e50e4e4aef0c225d7da58ee26a432042459ed5
Lepris <karolina.jankowska@gmail.com>
2019-05-16 20:27:57 +0100

    Login box Guest Login option

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -50,13 +50,14 @@ class LoginBox extends Component {
                 }
 
             })
+    }
 
-
-
-
-
-
-
+    handleGuestSubmit = (ev) => {
+        const { userLogin } = this.props
+        ev.preventDefault()
+        this.setState({ username: 'Guest' })
+        userLogin('Guest')
+        this.navigateBack()
     }
 
     render() {
@@ -65,15 +66,28 @@ class LoginBox extends Component {
             <>
                 <div className='ThreeColumnContainer'>
                     <div className='SingleArticleCard'>
-                        <form className='form_login' onSubmit={this.formBehaviour}>
-                            <h3 className='SteelBlue'><span className='Teal'>Log In</span> To add New Articles and Comments</h3>
-                            {this.state.nameErr && <span className='login_message'>Your Username is incorrect :(</span>}
-                            <label className='label_login' htmlFor='inputName'>Username: </label><input type='text' name='inputName' onChange={this.handleUserChange} placeholder='Username' ></input>
-                            {this.state.passwdErr && <p>Your Password is incorrect :(</p>}
+                        <h3 className='SteelBlue'><span className='Teal'>Log In</span> To add New Articles and Comments</h3>
+                        <div className='TwoColumns'>
 
-                            <label className='label_login' htmlFor='password'>Password:</label><input type='password' name='password' onChange={this.handlePasswDChange} placeholder='Password' ></input>
-                            <button className='LoginSubmit' onClick={this.handleSubmit}>Submit</button>
-                        </form>
+                            <div className='form_login'>
+                                <h3>Continue as Guest</h3>
+                                <p>This is only an exercise version, there is a plan to implement Auth0 in near future,
+                                     but for now and to save your time I have enabled this Guest Login option.
+                                     Please use this option to fully experience all editing options.</p>
+                                <button className='LoginSubmit' onClick={this.handleGuestSubmit}>Guest User</button>
+                            </div>
+                            <div className='form_login'>
+                                <h3>Registered User Login</h3>
+                                {this.state.nameErr && <span className='login_message'>Your Username is incorrect :(</span>}
+                                <label className='label_login' htmlFor='inputName'>Username: </label><input type='text' name='inputName' onChange={this.handleUserChange} placeholder='Username' ></input>
+                                {this.state.passwdErr && <p>Your Password is incorrect :(</p>}
+
+                                <label className='label_login' htmlFor='password'>Password:</label><input type='password' name='password' onChange={this.handlePasswDChange} placeholder='Password' ></input>
+                                <button className='LoginSubmit' onClick={this.handleSubmit}>Submit</button>
+
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </>
