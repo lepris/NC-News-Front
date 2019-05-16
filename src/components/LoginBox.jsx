2ac2c44bcce67f0a28bb4b0dadf1f7d193ed504f
Lepris <karolina.jankowska@gmail.com>
2019-05-16 19:17:34 +0100

    Login box improved

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -23,19 +23,39 @@ class LoginBox extends Component {
     formBehaviour = (ev) => {
         ev.preventDefault()
     }
+    navigateBack = () => {
+        window.history.back()
+    }
 
     handleSubmit = (ev) => {
         const { userLogin } = this.props
         ev.preventDefault()
         fetchUserByUsername(this.state.inputName)
-            .then(user => user.username === this.state.inputName ? user : 0)
-            .catch(err => this.setState({ nameErr: true, inputPassword: '' }))
+            .then(user => {
+                if (user.username === this.state.inputName && user.password === this.state.inputPassword) {
+                    this.setState({ username: this.state.inputName })
+                    userLogin(user.username)
+                } else if (user.username !== this.state.inputName) {
+                    this.setState({ nameErr: true })
+                } else if (user.password !== this.state.inputPassword) {
+                    this.setState({ passwdErr: true, inputPassword: '' })
+                }
 
-        fetchUserByUsername(this.state.inputName)
 
-            .then(user => user.password === this.state.inputPassword ? userLogin(user.username) : 0)
-            .catch(err => this.setState({ passwdErr: true }))
-        window.history.back()
+            })
+            .catch(err => this.setState({ nameErr: true, passwdErr: true, inputPassword: '' }))
+            .then(() => {
+                if (this.state.username) {
+                    this.navigateBack()
+                }
+
+            })
+
+
+
+
+
+
 
     }
 
@@ -46,13 +66,13 @@ class LoginBox extends Component {
                 <div className='ThreeColumnContainer'>
                     <div className='SingleArticleCard'>
                         <form className='form_login' onSubmit={this.formBehaviour}>
-                            <h3>LOG IN:</h3>
+                            <h3 className='SteelBlue'><span className='Teal'>Log In</span> To add New Articles and Comments</h3>
                             {this.state.nameErr && <span className='login_message'>Your Username is incorrect :(</span>}
-                            <label className='label_login' htmlFor='inputName'>username: </label><input type='text' name='inputName' onChange={this.handleUserChange} placeholder='Username' ></input>
-                            {this.state.passErr && <p>Your Password is incorrect :(</p>}
+                            <label className='label_login' htmlFor='inputName'>Username: </label><input type='text' name='inputName' onChange={this.handleUserChange} placeholder='Username' ></input>
+                            {this.state.passwdErr && <p>Your Password is incorrect :(</p>}
 
-                            <label className='label_login' htmlFor='password'>password:</label><input type='password' name='password' onChange={this.handlePasswDChange} placeholder='Password' ></input>
-                            <button onClick={this.handleSubmit}>Submit</button>
+                            <label className='label_login' htmlFor='password'>Password:</label><input type='password' name='password' onChange={this.handlePasswDChange} placeholder='Password' ></input>
+                            <button className='LoginSubmit' onClick={this.handleSubmit}>Submit</button>
                         </form>
                     </div>
                 </div>
