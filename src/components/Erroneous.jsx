fa2c284967810c8f3f552ba18d8db24bfe67ae91
Lepris <karolina.jankowska@gmail.com>
2019-05-17 15:04:54 +0100

    better error message

--- src/components/Erroneous.jsx
+++ src/components/Erroneous.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import errorIllustration from '../db/img/error.jpg';
 import './errors.css';
 
 
 export const Erroneous = ({ message }) => {
+    console.log(message)
     return (
-        <div className='container'>
-            <div className='image'>
-                <img height='50vh' alt='Life of Brian' src={errorIllustration} />
-            </div>
-            <div className='text_part'>
-                <h2>Big Fat Error</h2>
+        <div className='ThreeColumnContainer'>
+            < div className='SingleArticleCard' >
 
-                <h4>{message}</h4>
-                <p>Some things in life are bad,</p>
-                <p>They can really make you mad,</p>
-                <p>Other things just make you swear and curse,</p>
-                <p>When you're chewing life's gristle,</p>
-                <p>Don't grumble,</p>
-                <p>Give a whistle</p>
-                <p><small>-Monthy Python</small></p>
+                <div className='text_part'>
+                    <h3 className='MidnightBlue'>Something went wrong</h3>
+                    <h4 className='Teal'>{message.status}</h4>
 
-            </div>
+                    <p>Some things in life are bad,</p>
+                    <p>They can really make you mad,</p>
+                    <p>Other things just make you swear and curse,</p>
+                    <p>When you're chewing life's gristle,</p>
+                    <p>Don't grumble,</p>
+                    <p>Give a whistle</p>
+                    <p><small>-Monthy Python</small></p>
+
+                </div>
+            </div >
         </div >
     )
 }
